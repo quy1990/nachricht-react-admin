fed2c07c8e36fa52245bcd9105e4558040988d80
tam@gmail.com <tam@gmail.com>
2021-10-30 17:41:04 +0200

    add StatusesPage.jsx and adjust route

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -7,6 +7,7 @@ import UsersPage from '../pages/UsersPage'
 import PostsPage from '../pages/PostsPage'
 import CategoriesPage from '../pages/CategoriesPage'
 import TagsPage from '../pages/TagsPage'
+import Statuses from '../pages/StatusesPage'
 import RolesPage from '../pages/RolesPage'
 import CommentsPage from '../pages/CommentsPage'
 import UserProfilePage from '../pages/UserProfilePage'
@@ -14,14 +15,15 @@ import UserProfilePage from '../pages/UserProfilePage'
 const Routes = () => {
     return (
         <Switch>
-            <Route path='/' exact component={Dashboard}/>
+            <Route path='/tags' component={TagsPage}/>
             <Route path='/users' component={UsersPage}/>
             <Route path='/posts' component={PostsPage}/>
-            <Route path='/categories' component={CategoriesPage}/>
-            <Route path='/tags' component={TagsPage}/>
             <Route path='/roles' component={RolesPage}/>
+            <Route path='/' exact component={Dashboard}/>
+            <Route path='/statuses' component={Statuses}/>
             <Route path='/comments' component={CommentsPage}/>
             <Route path='/profile' component={UserProfilePage}/>
+            <Route path='/categories' component={CategoriesPage}/>
         </Switch>
     )
 }
---
+++ src/pages/StatusesPage.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Table from '../components/table/Table'
+
+const customerTableHead = [
+    '',
+    'name',
+    "actions"
+]
+
+const renderHead = (item, index) => <th key={index}>{item}</th>
+
+const renderBody = (item, index) => (
+    <tr key={index}>
+        <td>{item.id}</td>
+        <td>{item.name}</td>
+        <td>action1/action2</td>
+    </tr>
+)
+
+const StatusesPage = () => {
+    let url = "/statuses";
+
+    return (
+        <div>
+            <h2 className="page-header">
+                Statuses
+            </h2>
+            <div className="row">
+                <div className="col-12">
+                    <div className="card">
+                        <div className="card__body">
+                            {
+                                <Table
+                                    limit='10'
+                                    headData={customerTableHead}
+                                    renderHead={(item, index) => renderHead(item, index)}
+                                    url={url}
+                                    renderBody={(item, index) => renderBody(item, index)}
+                                />
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StatusesPage
