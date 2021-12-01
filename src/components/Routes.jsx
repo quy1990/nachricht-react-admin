a875cc1cc8bbf82f3ebe6721a9437522073c2442
Tu Quy Nguyen <nguyentuquy2008@gmail.com>
2021-12-01 22:11:16 +0100

    try to add token in request

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Dashboard from '../pages/Dashboard'
+import LoginPage from '../pages/LoginPage'
 import UsersPage from '../pages/UsersPage'
 import PostsPage from '../pages/PostsPage'
 import CategoriesPage from '../pages/CategoriesPage'
@@ -18,6 +19,7 @@ const Routes = () => {
             <Route path='/nachricht-react-admin/posts' component={PostsPage}/>
             <Route path='/nachricht-react-admin/roles' component={RolesPage}/>
             <Route path='/nachricht-react-admin/' exact component={Dashboard}/>
+            <Route path='/nachricht-react-admin/login' exact component={LoginPage}/>
             <Route path='/nachricht-react-admin/statuses' component={StatusesPage}/>
             <Route path='/nachricht-react-admin/comments' component={CommentsPage}/>
             <Route path='/nachricht-react-admin/profile' component={UserProfilePage}/>
